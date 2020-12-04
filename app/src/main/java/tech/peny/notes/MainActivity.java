5e106d68af2a3362f6f0e992ab82bdc61aba85eb
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-04 17:46:23 +0000

    [feat]: Added menu for adding notes

--- app/src/main/java/tech/peny/notes/MainActivity.java
+++ app/src/main/java/tech/peny/notes/MainActivity.java
@@ -1,9 +1,13 @@
 package tech.peny.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,6 +18,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     ListView listView;
     static ArrayList<String> notes = new ArrayList<>();
+    static  ArrayAdapter arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         notes.add("Example Note");
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,notes);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,notes);
         listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -35,6 +40,30 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+
+    }
+
+
+//    Menu code
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = new MenuInflater(getApplicationContext());
+        menuInflater.inflate(R.menu.add_note_menu,menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.add_note){
+            Intent intent = new Intent(getApplicationContext(),NoteEditorActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
 
     }
 }
--- app/src/main/java/tech/peny/notes/NoteEditorActivity.java
+++ app/src/main/java/tech/peny/notes/NoteEditorActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 
 public class NoteEditorActivity extends AppCompatActivity {
@@ -21,5 +23,23 @@ public class NoteEditorActivity extends AppCompatActivity {
             editText.setText(MainActivity.notes.get(noteId));
 
         }
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.notes.set(noteId,String.valueOf(s));
+
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
