fb3792dd9f91ccaadca8daf21fe03d2c324cada3
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-04 17:34:37 +0000

    [feat]: Added activity for editing notes
    passing notes from activity to the other

--- app/src/main/java/tech/peny/notes/MainActivity.java
+++ app/src/main/java/tech/peny/notes/MainActivity.java
@@ -2,13 +2,39 @@ package tech.peny.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listView;
+    static ArrayList<String> notes = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        notes.add("Example Note");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,notes);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),NoteEditorActivity.class);
+                intent.putExtra("nodeId",position);
+                startActivity(intent);
+
+            }
+        });
+
+
+
     }
 }
